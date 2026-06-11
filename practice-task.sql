db595359be88ebb86fc63f86c034a2b7ee594135
Alamin4D <alaminahmed79000@gmail.com>
2026-06-11 14:44:42 +0600

    feat: implement INNER, LEFT, RIGHT, and FULL JOIN examples

--- practice-task.sql
+++ practice-task.sql
@@ -84,7 +84,6 @@ select * from courses
 
 
 
-
 -- -- Display all students and their phone numbers.
 -- If the phone number is NULL, show 'Not Provided' using COALESCE.
 select first_name, last_name, coalesce(phone, 'Not Privided') as phone from students;
